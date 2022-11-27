9f5b0e0c6d0b1cbaf4b42991f9bd4b5e03d17344
khairul ahmed <md.raju130@yahoo.com>
2022-11-27 09:32:44 -0800

    my orders

--- src/My Orders/MyOrders.js
+++ src/My Orders/MyOrders.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../contexts/AuthProvider';
 
 const MyOrders = () => {
+
+    const {user} = useContext(AuthContext)
+    const{email} = user
+
+    const [myProducts, setMyProduct] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/bookings/${email}`)
+            .then(res => res.json())
+            .then(data => {
+                
+                console.log(data)
+                setMyProduct(data)
+            })
+            .catch((err) => toast.error(err.message))
+  
+    }, [email]);
     return (
         <div>
-            my orders
+        <div className='container mx-auto my-10'>
+            <div className=" gap-10 mx-auto px-4 grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-2 ">
+                {
+                    myProducts.map((myProduct) => {
+                        return (
+                            <div>
+                                <div className="card  bg-base-100 shadow-xl">
+                                    <div className="card-body w-84 h-96">
+                                        <h2 className="card-title">Name: {myProduct.name}</h2>
+                                        <h2 className="card-title"> Name: {myProduct.itemName}</h2>
+                                 
+                                        <h2 className="card-title">Location: {myProduct.location}</h2>
+                                        <h2 className="card-title">Price: {myProduct.price}</h2>
+                                        <h2 className="card-title">: {myProduct.used}</h2>
+                                   
+                                        <h2 className="card-title">Description: {myProduct.description}</h2>
+                                        <h2 className="card-title">Mobile: {myProduct.phone}</h2>
+                              
+                                        
+                                        <p></p>
+                      
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }) 
+                }
+            </div>
         </div>
+    </div>
     );
 };
 
