ce5002dbdbd269d6e02897f4182804d42f92c25f
khairul ahmed <md.raju130@yahoo.com>
2022-11-27 09:52:53 -0800

    deelete added

--- src/My Orders/MyOrders.js
+++ src/My Orders/MyOrders.js
@@ -4,8 +4,8 @@ import { AuthContext } from '../contexts/AuthProvider';
 
 const MyOrders = () => {
 
-    const {user} = useContext(AuthContext)
-    const{email} = user
+    const { user } = useContext(AuthContext)
+    const { email } = user
 
     const [myProducts, setMyProduct] = useState([])
 
@@ -13,45 +13,67 @@ const MyOrders = () => {
         fetch(`http://localhost:5000/bookings/${email}`)
             .then(res => res.json())
             .then(data => {
-                
+
                 console.log(data)
                 setMyProduct(data)
             })
             .catch((err) => toast.error(err.message))
-  
+
     }, [email]);
+
+
+
+
+    const handleDelete = (id) => {
+   
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+
+                if (data.success) {
+                    toast.success(data.message);
+                } else {
+                    toast.error(data.error);
+                }
+            }).catch(err => toast.error(err.message))
+    };
+
+
+
     return (
         <div>
-        <div className='container mx-auto my-10'>
-            <div className=" gap-10 mx-auto px-4 grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-2 ">
-                {
-                    myProducts.map((myProduct) => {
-                        return (
-                            <div>
-                                <div className="card  bg-base-100 shadow-xl">
-                                    <div className="card-body w-84 h-96">
-                                        <h2 className="card-title">Name: {myProduct.name}</h2>
-                                        <h2 className="card-title"> Name: {myProduct.itemName}</h2>
-                                 
-                                        <h2 className="card-title">Location: {myProduct.location}</h2>
-                                        <h2 className="card-title">Price: {myProduct.price}</h2>
-                                        <h2 className="card-title">: {myProduct.used}</h2>
-                                   
-                                        <h2 className="card-title">Description: {myProduct.description}</h2>
-                                        <h2 className="card-title">Mobile: {myProduct.phone}</h2>
-                              
-                                        
-                                        <p></p>
-                      
+            <p className='text-2xl '>My Orders</p>
+            <div className='container mx-auto my-10'>
+                <div className=" gap-10 mx-auto px-4 grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-2 ">
+
+                    {
+                        myProducts.map((myProduct) => {
+
+                            return (
+                                <div>
+                                    <div className="card  bg-base-100 shadow-xl">
+                                        <div className="card-body w-84 h-84">
+                                            <h2 className="card-title">Name: {myProduct.name}</h2>
+                                            <h2 className="card-title"> Name: {myProduct.itemName}</h2>
+                                            <h2 className="card-title">Location: {myProduct.location}</h2>
+                                            <h2 className="card-title">Price: {myProduct.price}</h2>
+                                            <h2 className="card-title">Mobile: {myProduct.phone}</h2>
+                                            <button onClick={()=>handleDelete(myProduct._id)} className='btn btn-accent'>Delete</button>
+
+
+                                            <p></p>
+
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    }) 
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
